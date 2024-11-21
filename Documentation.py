34d289e89b1f957cc6e13fb206224dca2d0ba6c7
Kumar Priyanshu <priyanshukumar.0309@gmail.com>
2024-11-21 15:56:58 +0100

    Update Documentation.py

--- Documentation.py
+++ Documentation.py
@@ -17,7 +17,7 @@ def app():
             1. Log in to your Notion account.
             2. Visit the [Notion API Integration Page](https://www.notion.so/my-integrations).
             3. Create a new integration and copy the generated API key.
-        st.info(""" If you don't have the required permissions to create these keys, please ask your administrator to generate them for you.""")
+        st.info("If you don't have the required permissions to create these keys, please ask your administrator to generate them for you.")
         - **Azure DevOps Personal Access Token (PAT)**:
              1. Navigate to your Azure DevOps organization.
             2. Go to your **Profile Settings** by clicking on your profile picture.
