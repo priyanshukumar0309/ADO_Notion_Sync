8513d73f1a73f18f4ff263e1a00726964dbd3a05
Kumar Priyanshu <priyanshukumar.0309@gmail.com>
2024-11-22 03:11:50 +0100

    Update Sync_and_Actions.py

--- Sync_and_Actions.py
+++ Sync_and_Actions.py
@@ -30,11 +30,11 @@ def app():
     with st.expander("## Sync Flow"):
         st.image('images/ADO_Notion_Sync.png',width=1800)
     st.write("""
-            - #### This page shows comparison and actions for Notion and Azure Devops fetched data.
+            - ##### This page shows comparison and actions for Notion and Azure Devops fetched data.
             - ##### You can push items from ADO to Notion.
-            - ### You can push items from Notion to ADO.
-            - ### You can sync between existing items.
-            - ### You can add update descriptions at both places without overwriting. 
+            - ##### You can push items from Notion to ADO.
+            - ##### You can sync between existing items.
+            - ##### You can add update descriptions at both places without overwriting. 
         """)
     st.markdown("<hr>", unsafe_allow_html=True)
 
